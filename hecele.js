f2151b887162f4c13c195ea1be4dc52122d70f40
bilgiprog <teksalx@gmail.com>
2016-11-06 15:16:09 +0300

    ptrn fix

--- hecele.js
+++ hecele.js
@@ -74,7 +74,7 @@ function hecele(klme)
     { ptrn: /^[aeiouöüıİ][^aeiouöüıİ0-9][^aeiouöüıİ0-9]($|[^aeiouöüıİ0-9])/i,  uzn: 3 },
     
     { ptrn: /^[^aeiouöüıİ0-9][aeiouöüıİ][^aeiouöüıİ0-9][aeiouöüıİ]/i,  uzn: 2 },
-    { ptrn: /^[^aeiouöüıİ0-9][aeiouöüıİ][aeiouöüıİ0-9]/i,  uzn: 2 },
+    { ptrn: /^[^aeiouöüıİ0-9][aeiouöüıİ][aeiouöüıİ]/i,  uzn: 2 },
     { ptrn: /^[^aeiouöüıİ0-9][aeiouöüıİ][^aeiouöüıİ0-9]($|[^aeiouöüıİ0-9][aeiouöüıİ])/i,  uzn: 3 },
     { ptrn: /^[^aeiouöüıİ0-9][aeiouöüıİ][^aeiouöüıİ0-9][^aeiouöüıİ0-9]($|[^aeiouöüıİ0-9])/i,  uzn: 4 },
     { ptrn: /^[^aeiouöüıİ0-9][^aeiouöüıİ0-9][aeiouöüıİ][^aeiouöüıİ0-9]($|[^aeiouöüıİ0-9])/i,  uzn: 4 }          
