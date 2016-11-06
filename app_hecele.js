267c4aa102fba42f7172b09f27f03fb9adcef49d
bilgiprog <teksalx@gmail.com>
2016-11-06 13:23:30 +0300

    heceleme ekle

---
+++ app_hecele.js
@@ -0,0 +1,37 @@
+window.addEventListener("load", function() {
+    document.getElementById("hcl-btn").onclick = function() {
+        var klme = document.getElementById("kelime").value;
+        if (/[^a-zşğçöüİı]/i.test(klme))
+            document.getElementById("heceli").innerHTML = "Sadece harflerden oluşan bir kelime girin."
+        else
+            document.getElementById("heceli").innerHTML = hecele(klme);
+    }
+
+    function hecele(klme) {
+      
+      if (klme.length < 3)
+        return klme;
+      
+      const hc = [
+        { ptrn: /^[aeiouöüıİ][^aeiouöüıİ0-9][aeiouöüıİ]/i,  uzn: 1 },
+        { ptrn: /^[aeiouöüıİ][^aeiouöüıİ0-9][^aeiouöüıİ0-9][aeiouöüıİ]/i,  uzn: 2 },
+        { ptrn: /^[aeiouöüıİ][^aeiouöüıİ0-9][^aeiouöüıİ0-9]($|[^aeiouöüıİ0-9])/i,  uzn: 3 },
+        
+        { ptrn: /^[^aeiouöüıİ0-9][aeiouöüıİ][^aeiouöüıİ0-9][aeiouöüıİ]/i,  uzn: 2 },
+        { ptrn: /^[^aeiouöüıİ0-9][aeiouöüıİ][^aeiouöüıİ0-9]($|[^aeiouöüıİ0-9][aeiouöüıİ])/i,  uzn: 3 },
+        { ptrn: /^[^aeiouöüıİ0-9][aeiouöüıİ][^aeiouöüıİ0-9][^aeiouöüıİ0-9]($|[^aeiouöüıİ0-9])/i,  uzn: 4 },
+        { ptrn: /^[^aeiouöüıİ0-9][^aeiouöüıİ0-9][aeiouöüıİ][^aeiouöüıİ0-9]($|[^aeiouöüıİ0-9])/i,  uzn: 4 }          
+      ];
+      
+        var i = -1;
+        while (++i < hc.length) 
+            if (hc[i].ptrn.test(klme))
+                if (klme.length > hc[i].uzn)
+                    return klme.substr(0, hc[i].uzn) + "-" + hecele(klme.substr(hc[i].uzn));
+                else
+                    return klme;
+
+        return klme; // hiç match yok, olduğu gibi döndür
+    }  
+
+});
